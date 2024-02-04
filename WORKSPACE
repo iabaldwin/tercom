94dbc0f99dd8b7cde01220f07f7dd4b7ce3af482
Ian Baldwin <ian@iabaldwin.com>
2024-02-04 10:00:57 -0800

    Initial commit

---
+++ WORKSPACE
@@ -0,0 +1,5 @@
+new_local_repository(
+  name = "raylib",
+  path = "thirdparty/raylib",
+  build_file = "thirdparty/raylib/BUILD.bazel",
+)
---
+++ tercom.cc
@@ -0,0 +1,102 @@
+#include <cmath>
+#include <iostream>
+#include "raylib.h"
+
+void draw_world() {
+  DrawSphereWires({0, 0, 0}, 10.f, 25, 25, LIGHTGRAY);
+}
+
+struct System {
+  System() : x{0}, y{0}, z{0} {}
+  float x;
+  float y;
+  float z;
+  std::string reference{""};
+};
+
+void draw_coordinate_system(const System & system) {
+  Vector3 centre{system.x, system.y, system.z};
+  Vector3 X{0, 0, 1};
+  DrawLine3D(centre, X, RED);
+  Vector3 Y{0, 1, 0};
+  DrawLine3D(centre, Y, GREEN);
+  Vector3 Z{1, 0, 0};
+  DrawLine3D(centre, Z, BLUE);
+}
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+    DisableCursor();                    // Limit cursor to relative movement inside the window
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera, CAMERA_FREE);
+
+        if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                //DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+                //DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+                //DrawGrid(10, 1.0f);
+                draw_world();
+
+                System s;
+                draw_coordinate_system(s);
+
+            EndMode3D();
+
+            DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines( 10, 10, 320, 133, BLUE);
+
+            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+            DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY);
+            //DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
+            DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
